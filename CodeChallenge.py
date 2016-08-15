e099650e8a42d970d7de114b1f05adf9ec463b41
chun zheng <zhengc1@appstate.edu>
2016-08-15 16:52:35 -0400

    up to pep8 standards

--- CodeChallenge.py
+++ CodeChallenge.py
@@ -10,6 +10,7 @@ import re
 import json
 import pprint
 import requests
+import subprocess
 
 
 class CodeChallenge(object):
@@ -21,13 +22,17 @@ class CodeChallenge(object):
         # replace the spaces in the search input with '+' for the query string
         item = search.replace(' ', '+')
 
+        subprocess.Popen(["python", "loading_cursor.py"])
+
         # global api key
         self.apiKey = '&apiKey=9s7yx3b5sxchpmb8szzefu9x'
         # desired format
         return_format = '&format=json'
 
         # request url
-        search_request = 'http://api.walmartlabs.com/v1/search?query=' + item + return_format + self.apiKey
+        search_request = 'http://api.walmartlabs.com/v1/search?query='
+        + item + return_format + self.apiKey
+
         self.reviews_request = 'http://api.walmartlabs.com/v1/reviews/'
         self.recommendation_request = 'http://api.walmartlabs.com/v1/nbp?'
 
@@ -45,49 +50,60 @@ class CodeChallenge(object):
     def recommendations(self):
         # take the first item from the search to build the URI
         first_item = '&itemId=' + str(self.data['items'][0]['itemId'])
-        recommend_request = self.recommendation_request + self.apiKey + first_item
+
+        recommend_request = self.recommendation_request
+        + self.apiKey + first_item
 
         # Recommendation API URI
         recommendation_resp = requests.get(recommend_request)
 
         self.recommendations = recommendation_resp.json()
         for i in range(11):
-            self.reviews[str(self.recommendations[i]['itemId'])] = self.recommendations[i]['name']
-            self.products[str(self.recommendations[i]['itemId'])] = self.recommendations[i]['name']
+            self.reviews[str(self.recommendations[i]['itemId'])]
+            = self.recommendations[i]['name']
+
+            self.products[str(self.recommendations[i]['itemId'])]
+            = self.recommendations[i]['name']
 
     def review_text(self):
 
         # prints the overall ratings for the initial recommendations
         for key in self.reviews:
             # Review API URI
-            reviews_resp = requests.get(self.reviews_request + str(key) + '?' + self.apiKey)
+            reviews_resp = requests.get(
+                self.reviews_request + str(key) + '?' + self.apiKey
+            )
+
             reviews_json = reviews_resp.json()
 
             # Long string containing first 5 reviews (if they exist)
             review = ''
             for i in range(5):
-               review = review + reviews_json['reviews'][i]['reviewText']
+                review = review + reviews_json['reviews'][i]['reviewText']
             self.reviews[key] = review
 
     def sentiment_analysis(self):
         for key in self.reviews:
             string = self.reviews[key]
             text = re.sub(r'[^\w]', ' ', string)
-            req = requests.post("http://text-processing.com/api/sentiment/", data={'text': text})
+            req = requests.post(
+                "http://text-processing.com/api/sentiment/",
+                data={'text': text}
+            )
+
             sentiment_json = req.json()
             self.sentiment_review[key] = sentiment_json['probability']['pos']
-            self.answer[self.products[key]] = sentiment_json['probability']['pos'] 
+            self.answer[self.products[key]] =
+            sentiment_json['probability']['pos']
 
     def print_recommendations(self):
-        for key, value in sorted(self.answer.iteritems(), key=lambda (k,v): (v,k), reverse=True):
-            print "Name: %s | Pos Rating: %s" %(key, self.answer[key])
-
-    import itertools, sys
-    spinner = itertools.cycle(['-', '/', '|', '\\'])
-    while True:
-        sys.stdout.write(spinner.next())  # write the next character
-        sys.stdout.flush()                # flush stdout buffer (actual character display)
-        sys.stdout.write('\b')            # erase the last written char
+        print 'cat'
+        for key, value in sorted(
+            self.answer.iteritems(),
+            key=lambda (k, v): (v, k),
+            reverse=True
+        ):
+            print "Name: %s | Pos Rating: %s" % (key, self.answer[key])
 
 
 CodeChallenge = CodeChallenge()
